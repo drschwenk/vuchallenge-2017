1e67a7c429bd8e136b858f089de604eab9149991
drschwenk <schwgit2@gmail.com>
2017-03-17 16:36:52 -0700

    major restructuring, using as much common code as possible

---
+++ tqa/scoring/evaluation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+from task_evaluation.score_submission import score
+
+
+def main():
+    _, input_dir, output_dir = sys.argv
+    score(input_dir, output_dir)
+
+
+if __name__ == '__main__':
+    main()
